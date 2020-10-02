12938ba320adfe61bb364ccd0825645e0eb2ce9b
Jorge O. Uribe <jouribepe@gmail.com>
2020-10-02 13:12:32 -0500

    Set public path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,12 @@
 let mix = require('laravel-mix')
 
 mix
+    .setPublicPath('dist/')
     .js('src/js/app.js', 'dist/js')
     .postCss('src/css/app.css', 'dist/css', [
         require('postcss-import')
     ])
+    .version()
     .options({
         processCssUrls: false
     })
