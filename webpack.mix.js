1a3df65d907fb9541d4e413997e93d79c664420a
Jorge O. Uribe <jouribepe@gmail.com>
2020-10-05 11:50:44 -0500

    Twig support added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,23 @@
 let mix = require('laravel-mix')
 
+require('laravel-mix-twig-to-html')
+
 mix
     .setPublicPath('dist/')
     .js('src/js/app.js', 'dist/js')
     .postCss('src/css/app.css', 'dist/css', [
         require('postcss-import')
     ])
+    .twigToHtml({
+        files: 'src/views/**/*.{twig,html}',
+        fileBase: 'src/views',
+        twigOptions: {
+            namespaces: {
+                'layouts': 'src/views/_layouts',
+                'components': 'src/views/_components',
+            }
+        }
+    })
     .options({
         processCssUrls: false
     })
