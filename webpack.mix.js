6586c1424fe46625fb18ddd20a8f454c2e52885b
Jorge O. Uribe <jouribepe@gmail.com>
2020-10-05 22:12:38 -0500

    Eslint added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,17 @@
-let mix = require('laravel-mix')
+const mix = require('laravel-mix')
 
 require('laravel-mix-twig-to-html')
 
-mix
-    .setPublicPath('dist/')
+mix.setPublicPath('dist/')
     .js('src/js/app.js', 'dist/js')
-    .postCss('src/css/app.css', 'dist/css', [
-        require('postcss-import')
-    ])
+    .postCss('src/css/app.css', 'dist/css', [require('postcss-import')])
     .twigToHtml({
         files: 'src/views/**/*.{twig,html}',
         fileBase: 'src/views',
         twigOptions: {
             namespaces: {
-                'layouts': 'src/views/_layouts',
-                'components': 'src/views/_components',
+                layouts: 'src/views/_layouts',
+                components: 'src/views/_components'
             }
         }
     })
